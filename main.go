c85e9ad5d472984b8b84fe99ba9602642766082d
Frederic Pinaud <fpinaud17@gmail.com>
2020-02-18 16:42:38 +0100

    test -> all in the same file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/FredPi17/terraform-provider-hello/hello"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -12,4 +13,61 @@ func main() {
 			return hello.Provider()
 		},
 	})
+}
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		/*Schema: map[string]*schema.Schema {
+		    "nom": &schema.Schema {
+		        Type: schema.TypeString,
+		        Description: "Your name",
+		        Required: true,
+		    },
+		},*/
+		DataSourcesMap: map[string]*schema.Resource{
+			"hello_world": resourceServer(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+
+	return nil, nil
+}
+
+func resourceServer() *schema.Resource {
+
+	return &schema.Resource{
+		Create: resourceServerCreate,
+		Read:   resourceServerRead,
+		Update: resourceServerUpdate,
+		Delete: resourceServerDelete,
+
+		Schema: map[string]*schema.Schema{
+			"nom": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
+	nom := d.Get("nom").(string)
+	d.SetId(nom)
+	return resourceServerCreate(d, m)
+}
+
+func resourceServerRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
+	return resourceServerRead(d, m)
+}
+
+func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
 }
