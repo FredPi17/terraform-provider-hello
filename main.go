b109d435171917645dbcb4f28c2a0291cf6efe6d
Frederic Pinaud <fpinaud17@gmail.com>
2020-02-18 17:12:29 +0100

    test with other config on main.go

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"github.com/FredPi17/terraform-provider-hello/hello"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return hello.Provider()
-		},
-	})
+		ProviderFunc: hello.Provider})
 }
