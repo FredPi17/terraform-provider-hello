23ae292fc8d32f88846e02b1ef3ca87931f44b78
Frederic Pinaud <fpinaud17@gmail.com>
2020-02-19 10:52:18 +0100

    test with packages

--- example.tf
+++ example.tf
@@ -3,15 +3,3 @@ provider "hello" {}
 resource "hello_world" "test1" {
   nom="test"
 }
-
-resource "hello_create" "test2" {
-  create = <<EOF
-  echo '{"test2":""test"}' >> test2.json
-  cat test2.json
-  EOF
-
-  environment = {
-    test2 = "test"
-  }
-
-}
--- main.go
+++ main.go
@@ -1,74 +1,15 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/FredPi17/terraform-provider-hello/hello"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	_ "reflect"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return hello.Provider()
 		},
 	})
-}
-
-func Provider() *schema.Provider {
-	return &schema.Provider{
-		ResourcesMap: map[string]*schema.Resource{
-			"hello_world": resourceServer(),
-		},
-
-		ConfigureFunc: providerConfigure,
-	}
-}
-
-func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	var client Client
-	return &client, nil
-}
-
-//Config is the config for the client.
-type Config struct {
-}
-
-//Client is the client itself. Since we already have access to the shell no real provisioning needs to be done
-type Client struct {
-}
-
-func resourceServer() *schema.Resource {
-	return &schema.Resource{
-		Create: resourceServerCreate,
-		Read:   resourceServerRead,
-		Update: resourceServerUpdate,
-		Delete: resourceServerDelete,
-
-		Schema: map[string]*schema.Schema{
-			"nom": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-		},
-	}
-}
-
-func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
-	nom := d.Get("nom").(string)
-	d.SetId(nom)
-	return resourceServerCreate(d, m)
-}
-
-func resourceServerRead(d *schema.ResourceData, m interface{}) error {
-	return nil
-}
-
-func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
-	return resourceServerRead(d, m)
-}
-
-func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
-	d.SetId("")
-	return nil
 }
